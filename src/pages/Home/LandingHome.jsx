e16778a0a6630b62a1728981b536f706a3738ad9
rodriguezdevin24 <131619471+rodriguezdevin24@users.noreply.github.com>
2023-07-27 00:07:25 -0400

    added more landing screen animations

--- src/pages/Home/LandingHome.jsx
+++ src/pages/Home/LandingHome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import './home.css'
+import "./home.css";
 
 const Section = styled.div`
   height: 100vh;
@@ -16,23 +16,35 @@ const Section = styled.div`
   scroll-snap-align: center;
 `;
 
-
-const Subtitle = styled.h2`
-  font-size: 1.2em;
-  margin-bottom: 2em;
-`;
-
+// const Subtitle = styled.h2`
+//   font-size: 1.2em;
+//   margin-bottom: 2em;
+// `;
 
 const h1EnterFromBottomVariants = {
   initial: {
-    y: "100vh",
+    y: "50vh",
     opacity: 0,
   },
   animate: {
     y: 0,
     opacity: 1,
-    transition: { delay: 2, duration: 1 }
-  }
+    transition: { delay: 1, duration: 1 },
+  },
+};
+const subtitleVariants = {
+  initial: {
+    opacity: 1,
+    y: "100vh",
+  },
+  in: {
+    opacity: 1,
+    y: 0,
+  },
+  out: {
+    opacity: 0,
+    y: "-100vh",
+  },
 };
 
 const LandingHome = () => {
@@ -46,11 +58,28 @@ const LandingHome = () => {
       >
         Hi. I'm Devin Rodriguez.
       </motion.h1>
-      <Subtitle>A Software Engineer.</Subtitle>
-      <p>
+      <motion.h2
+        className="landing-subtitle"
+        variants={subtitleVariants}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+          delay: 2.7,
+        }}
+      >
+        A Software Engineer.
+      </motion.h2>
+      <motion.p
+        initial={{ x: "-100%" }}
+        animate={{ x: "0%" }}
+        transition={{ duration: 1.3, ease: "easeInOut", delay: 3.8 }}
+      >
         From military medic to full-stack developer, I bring a unique,
         neurodivergent perspective and a passion for learning to my work.
-      </p>
+      </motion.p>
     </Section>
   );
 };
